211de335df4a923a01922b8a6e60206c3774b44f
ali eissa <aeissa.o@gmail.com>
2019-12-26 13:38:50 +0100

    Cache RDD.

--- wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
+++ wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
@@ -39,7 +39,7 @@ object WikipediaRanking {
   val sc: SparkContext = new SparkContext(conf)
   // Hint: use a combination of `sc.textFile`, `WikipediaData.filePath` and `WikipediaData.parse`
   val wikiRdd: RDD[WikipediaArticle] =
-    sc.textFile(WikipediaData.filePath).map(WikipediaData.parse)
+    sc.textFile(WikipediaData.filePath).map(WikipediaData.parse).cache()
 
   /** Returns the number of articles on which the language `lang` occurs.
     *  Hint1: consider using method `aggregate` on RDD[T].
